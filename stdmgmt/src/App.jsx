07a40daaef0009ec88566ab9852581ba7b07db58
bikuvai205 <itsmeking205@gmail.com>
2025-05-09 21:23:59 +0545

    tailwind css added

--- stdmgmt/src/App.jsx
+++ stdmgmt/src/App.jsx
@@ -2,7 +2,16 @@ import { useState } from 'react'
 function App() {
  return (
     <>
-      
+      <h1>
+        <span className="text-3xl font-bold underline">
+          Hello world!
+        </span>
+      </h1>
+      <h1>
+        <span className="text-3xl font-bold underline">
+          Hello world!
+        </span>
+      </h1>
     </>
   )
 }
